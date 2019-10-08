62534a871c1be705540c0457ae2df6891ab9f710
Maserall Marcelin <maserallm@knights.ucf.edu>
2019-10-07 21:40:40 -0400

    updates to index, js, css, and background image

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+let compNum = 0;
+let userScore = 0;
+let wins = 0;
+let losses= 0;
+
+let earth = $("#earth-element");
+let water = $("#water-element");
+let air = $("#air-element");
+let fire = $("#fire-element");
+
