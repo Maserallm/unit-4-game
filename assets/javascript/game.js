7f21165300e1fdb578f8670531593f5060ff0c16
Maserall Marcelin <maserallm@knights.ucf.edu>
2020-04-15 21:39:35 -0400

    added styling and cleaned up js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,132 +1,90 @@
-
-
 $(document).ready(function() {
+  let targetScore = Math.floor(Math.random() * 101) + 19;
 
-    let targetScore = Math.floor(Math.random() * 101) + 19;
-    
+  $("#target-number").html(targetScore);
+
+  let userScore = 0;
+
+  let wins = 0;
+  let loss = 0;
+
+  const earth = $("#earth-element");
+  const water = $("#water-element");
+  const air = $("#air-element");
+  const fire = $("#fire-element");
+  const victoryDefeat = $("#victory-defeat");
+
+  victoryDefeat.html("<h2>Lets Play!</h2>");
+
+  let randomNum1 = Math.floor(Math.random() * 11) + 1;
+  let randomNum2 = Math.floor(Math.random() * 11) + 1;
+  let randomNum3 = Math.floor(Math.random() * 11) + 1;
+  let randomNum4 = Math.floor(Math.random() * 11) + 1;
+
+  const winOrLoss = () => {
+    if (userScore === targetScore) {
+      wins++;
+      $("#wins").html("Wins: " + wins);
+      victoryDefeat.html("<h2>Victory!</h2>");
+
+      console.log("winner winner!");
+      reset();
+    } else if (userScore > targetScore) {
+      loss++;
+      $("#loss").html("Loss: " + loss);
+      victoryDefeat.html("<h2>Defeat!</h2>");
+      console.log("loser");
+      reset();
+    }
+  };
+
+  $(earth).on("click", () => {
+    userScore += randomNum1;
+    $("#user-number").html(userScore);
+    console.log(userScore);
+
+    winOrLoss();
+  });
+
+  $(water).on("click", () => {
+    userScore += randomNum2;
+    $("#user-number").html(userScore);
+    console.log(userScore);
+
+    winOrLoss();
+  });
+
+  $(air).on("click", () => {
+    userScore += randomNum3;
+    $("#user-number").html(userScore);
+    console.log(userScore);
+
+    winOrLoss();
+  });
+
+  $(fire).on("click", () => {
+    userScore += randomNum4;
+    $("#user-number").html(userScore);
+    console.log(userScore);
+
+    winOrLoss();
+  });
+
+  //reset
+  const reset = () => {
+    targetScore = Math.floor(Math.random() * 101) + 19;
+
     $("#target-number").html(targetScore);
 
-    let userScore = 0;
-    
-    let wins = 0;
-    let loss = 0;
+    userScore = 0;
 
-    let earth = $("#earth-element");
-    let water = $("#water-element");
-    let air = $("#air-element");
-    let fire = $("#fire-element");
+    $("#user-number").html(userScore);
 
-    let randomNum1 = Math.floor(Math.random() * 11) + 1;
-    let randomNum2 = Math.floor(Math.random() * 11) + 1;
-    let randomNum3 = Math.floor(Math.random() * 11) + 1;
-    let randomNum4 = Math.floor(Math.random() * 11) + 1;
+    victoryDefeat;
 
-    $(earth).on("click", function () {
-
-        userScore += randomNum1;
-        $("#user-number").html(userScore);
-        console.log(userScore);
-
-        if (userScore === targetScore) {
-            wins++;
-            $("#wins").html("Wins: " + wins);
-            $("#victory-defeat").text("<h2>Victory!</h2>")
-            console.log("winner winner!");
-            reset();
-        } else if (userScore > targetScore) {
-            loss++;
-            $("#loss").html("Loss: " + loss);
-            $("#victory-defeat").text("<h2>Defeat!</h2>")
-            console.log("loser")
-            reset();
-        }
-    });
-
-    $(water).on("click", function () {
-
-        userScore += randomNum2;
-        $("#user-number").html(userScore);
-        console.log(userScore);
-
-        if (userScore === targetScore) {
-            wins++;
-            $("#wins").html("Wins: " + wins);
-            $("#victory-defeat").html("<h2>Victory!</h2>")
-            console.log("winner winner!");
-            reset();
-        } else if (userScore > targetScore) {
-            loss++;
-            $("#loss").html("Loss: " + loss);
-            $("#victory-defeat").html("<h2>Defeat!</h2>")
-            console.log("loser")
-            reset();
-        }
-    });
-
-    $(air).on("click", function () {
-
-        userScore += randomNum3;
-        $("#user-number").html(userScore);
-        console.log(userScore);
-
-        if (userScore === targetScore) {
-            wins++;
-            $("#wins").html("Wins: " + wins);
-            $("#victory-defeat").html("<h2>Victory!</h2>")
-            console.log("winner winner!");
-            reset();
-        } else if (userScore > targetScore) {
-            loss++
-            $("#loss").html("Loss: " + loss);
-            $("#victory-defeat").html("<h2>Defeat!</h2>")
-            console.log("loser")
-            reset();
-        }
-    });
-
-    $(fire).on("click", function () {
-
-        userScore += randomNum4;
-        $("#user-number").html(userScore);
-        console.log(userScore);
-
-        if (userScore === targetScore) {
-            wins++;
-            $("#wins").html("Wins: " + wins);
-            $("#victory-defeat").html("<h2>Victory!</h2>")
-            console.log("winner winner!");
-            reset();
-
-        } else if (userScore > targetScore) {
-            loss++;
-            $("#loss").html("Loss: " + loss);
-            $("#victory-defeat").html("<h2>Defeat!</h2>")
-            console.log("loser")
-            reset();
-        }
-    });
-
-    //reset
-    function reset() {
-        targetScore = Math.floor(Math.random() * 101) + 19;
-    
-        $("#target-number").html(targetScore);
-        
-        userScore = 0;
-
-        $("#user-number").html(userScore);
-
-        $("#victory-defeat").html("<h2></h2>")
-
-        randomNum1 = Math.floor(Math.random() * 11) + 1;
-        randomNum2 = Math.floor(Math.random() * 11) + 1;
-        randomNum3 = Math.floor(Math.random() * 11) + 1;
-        randomNum4 = Math.floor(Math.random() * 11) + 1;
-    }
-
-
-
-
-
+    randomNum1 = Math.floor(Math.random() * 11) + 1;
+    randomNum2 = Math.floor(Math.random() * 11) + 1;
+    randomNum3 = Math.floor(Math.random() * 11) + 1;
+    randomNum4 = Math.floor(Math.random() * 11) + 1;
+  };
 });
-
